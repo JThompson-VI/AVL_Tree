6c3ff9fd17e75d947491cceb128fdb81139fa812
Jacob Thompson <thompsonjacob53@gmail.com>
2021-10-12 21:47:43 -0600

    finished implimentation of AvlTree

--- src/hw4_Q4.java
+++ src/hw4_Q4.java
@@ -63,30 +63,115 @@ class AvlTree{
     //please complete the following seven functions
 	private AvlNode insert(AvlNode v, int x)//this function is overloaded
 	{
+        if (v == null) {
+            return new AvlNode(x);
+        }
+        else if (x == v.key) {
+            return v;
+        }
+        else if (x < v.key) {
+            v.left = insert(v.left, x);
+        }
+        else {
+            v.right = insert(v.right, x);
+        }
+
+        return balance(v);
     }
 
     private AvlNode remove(AvlNode v, int x)//this function is overloaded
     {
+        if (v == null) return null;
+        // navigate to desired subtree
+        if (x > v.key) {
+            v.right = remove(v.right, x);
+            return balance(v);
+        }
+        else if (x < v.key) {
+            v.left = remove(v.left, x);
+            return balance(v);
+        }
+        else {
+            // if v has no children/ one child
+            if (v.left == null) {
+                return v.right;
+            }
+            else if (v.right == null) {
+                return v.left;
+            }
+            // case if v has two children
+            AvlNode subTree = v.right;
+            // navigate to the smallest node in the right subtree of v
+            while (subTree.left != null) {
+                subTree = subTree.left;
+            }
+            v.key = subTree.key;
+            v.right = remove(v.right, v.key);
+            return balance(v);
+        }
+
     }
+
 
     private AvlNode balance(AvlNode v)
     {
+        if (v == null) return null;
+
+        if (getHeight(v.left) - getHeight(v.right) > 1) {
+            if (getHeight(v.left.left) >= getHeight(v.left.right)) {
+                v = rightRotate(v);
+            }else {
+                v = doubleLeftRightRotate(v);
+            }
+        }
+        else if (getHeight(v.right) - getHeight(v.left) > 1) {
+            if (getHeight(v.right.right) >= getHeight(v.right.left)) {
+                v = leftRotate(v);
+            }else {
+                v = doubleRightLeftRotate(v);
+            }
+        }
+        if (getHeight(v.left) >= getHeight(v.right)) {
+            v.height = 1 + getHeight(v.left);
+        }else {
+            v.height = 1 + getHeight(v.right);
+        }
+        return v;
     }
 
     private AvlNode rightRotate (AvlNode v)
     {
+        AvlNode newRoot = v.left;
+        v.left = newRoot.right;
+        newRoot.right = v;
+        v.height--;
+        return newRoot;
     }
 
     private AvlNode leftRotate (AvlNode v)
     {
+
+        AvlNode newRoot = v.right;
+        v.right = newRoot.left;
+        newRoot.left = v;
+        v.height--;
+        return newRoot;
     }
 
     private AvlNode doubleLeftRightRotate (AvlNode v)
     {
+        v.left = leftRotate(v.left);
+        v = rightRotate(v);
+        balance(v.right);
+        return v;
     }
 
     private AvlNode doubleRightLeftRotate (AvlNode v)
     {
+        v.right = rightRotate(v.right);
+        v = leftRotate(v);
+        balance(v.left);
+        return v;
     }
 }
 
@@ -122,11 +207,11 @@ public class hw4_Q4 {
             }
         }
 
-        //print the pre-odrder traversal on the console/screen
+        //print the pre-order traversal on the console/screen
         System.out.println("The pre-order traversal list is: ");
         tree.preOrder();
 
-        //print the in-odrder traversal
+        //print the in-order traversal
         System.out.println("The in-order traversal list is: ");
         tree.inOrder();
 
